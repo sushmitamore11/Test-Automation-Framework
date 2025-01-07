7f974f10f710ded69afe19a63da739b03fcee732
Sushmita <moresushmita1100@gmail.com>
2025-01-07 10:11:36 +0530

    Updated the xpath

--- src/test/java/com/ui/pages/ShoppingCartPage.java
+++ src/test/java/com/ui/pages/ShoppingCartPage.java
@@ -8,7 +8,7 @@ import com.utility.BrowserUtility;
 public class ShoppingCartPage extends BrowserUtility{
 
 	private AddressPage addressPage;
-	private static final By PROCEED_TO_CHECKOUT_BUTTON_LOCATOR = By.xpath("//a[@title=\"Proceed to checkout\"]");
+	private static final By PROCEED_TO_CHECKOUT_BUTTON_LOCATOR = By.xpath("//p[contains(@class,\"cart_navigation\")]/a[@title=\"Proceed to checkout\"]");
 	
 	public ShoppingCartPage(WebDriver driver) {
 		super(driver);
